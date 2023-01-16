50b81df5e2e20eeb0d4c223947d443042f079b71
Greenhelm <andrew.b.sheils@gmail.com>
2023-01-16 18:52:52 -0500

    Add functions for adding, subtracting, multiplying, and dividing

---
+++ calculator.js
@@ -0,0 +1,42 @@
+////////Main Functions
+
+//Add Function
+
+const addNumbers = function(numberA, numberB)
+{
+    let addedNumber = numberA + numberB;
+
+    return addedNumber;
+}
+
+//Subtract Function
+
+const subtractNumbers = function(numberA, numberB)
+{
+    let subtractedNumber = numberA - numberB;
+
+    return subtractedNumber;
+}
+
+//Multiply Function
+
+const multiplyNumbers = function(numberA, numberB)
+{
+    let multipliedNumber = numberA * numberB;
+
+    return multipliedNumber;
+}
+
+//Divide Function
+
+const divideNumbers = function(numberA, numberB)
+{
+    let dividedNumber = numberA / numberB;
+
+    return dividedNumber;
+}
+
+
+//////////////////////
+
+
