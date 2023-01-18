66b35e37d216e472daf06aeb35507c31972fcb3e
Greenhelm <andrew.b.sheils@gmail.com>
2023-01-17 19:35:01 -0500

    add event listeners to number buttons to add their respective number to the display box, add some style to display box, change id's of buttons

--- calculator.js
+++ calculator.js
@@ -71,3 +71,82 @@ const operateNumbers = function(operator, numberA, numberB)
 
 }
 
+///////////////
+
+//Add Number to Screen Function
+const divScreen = document.querySelector('.displayScreen');
+
+const btn0 = document.querySelector("#zero");
+const btn1 = document.querySelector("#one");
+const btn2 = document.querySelector("#two");
+const btn3 = document.querySelector("#three");
+const btn4 = document.querySelector("#four");
+const btn5 = document.querySelector("#five");
+const btn6 = document.querySelector("#six");
+const btn7 = document.querySelector("#seven");
+const btn8 = document.querySelector("#eight");
+const btn9 = document.querySelector("#nine");
+
+const btnAdd = document.querySelector("#add");
+const btnSubtract = document.querySelector("#subtract");
+const btnMultiply = document.querySelector("#multiply");
+const btnDivide = document.querySelector("#divide");
+const btnEquals = document.querySelector("#equals");
+const btnClear = document.querySelector("#clear");
+
+
+btn0.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "0";
+});
+
+btn1.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "1";
+});
+
+btn2.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "2";
+});
+
+btn3.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "3";
+});
+
+btn4.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "4";
+});
+
+btn5.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "5";
+});
+
+btn6.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "6";
+});
+
+btn7.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "7";
+});
+
+btn8.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "8";
+});
+
+btn9.addEventListener("click", function () {
+
+    divScreen.textContent = divScreen.textContent + "9";
+});
+
+
+
+
+
+
